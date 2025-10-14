ccc19290537525edfd9b4a6f0f465893b27009b6
BenjaminC188 <benjaminc188@nycstudents.net>
2025-10-14 06:22:58 -0700

    Update test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+"""student = {
+    'name': 'Cadee',
+    'age' : 15,
+    'grades' : (80,90,100)
+}
+print(student['name'])"""
+
+item = {
+    'name': "TV",
+    'price': 429.99,
+    'department': "Electronics",
+    'description': "A big screen."
+}
+
+item = {
+    'name': "IPhone",
+    'price': 1199.99,
+    'department': "Electronics",
+    'description': "A small screen."
+}
+
+item = {
+    'name': "IPad",
+    'price': 254.99,
+    'department': "Electronics",
+    'description': "A medium screen."
+}
+
