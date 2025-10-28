ebe506a9f3d87bc5b44291dc4d3f264d026d874a
BenjaminC188 <benjaminc188@nycstudents.net>
2025-10-28 06:26:12 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-"""best_buy_items = [
+best_buy_items = [
 {
     'name': "TV",
     'price': 429.99,
@@ -19,62 +19,30 @@
     'department': "Electronics",
     'description': "A medium screen."
 }
-]"""
+]
 
-"""for index, item in enumerate(best_buy_items):
-    print(index, ":", item['name'])"""
-
-"""for index, item in enumerate(best_buy_items):
-    print(index, ":", item['name'])"""
-"""def cart():
-    cart = []
-    Total = ['price']
-    print(best_buy_items ['name'] ['price'] ['department'] ['description'])
-    print("purchase a item")
-    while input == ['TV']:
-        print(best_buy_items[0])
-        print("Do you want to add this item to cart?")
-    if input == 'yes':
-        cart.append(best_buy_items[0])
-    elif input == ['IPhone']:
-        print(best_buy_items[1])
-        print("Do you want to add this item to cart?")
-    if input == 'yes':
-        cart.append(best_buy_items[1])
-    elif input == ['IPad']:
-        print(best_buy_items[2])
-        print("Do you want to add this item to cart?")
-    if input == 'yes':
-        cart.append(best_buy_items[2])
-    elif input == ['Done']:
-        print (cart) and (Total)"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def machine(a, b, c):
-    q = int(input("Number of quarters 1- 1000"))
-    a = 0
-    b = 0
-    c = 0
-
-    while q >= 1:
         
+
+for index, item in enumerate(best_buy_items):
+        print(index, ":", item["name"])
+for index, item in enumerate(best_buy_items):
+            print(index, ":", item["price"])
+def cart():
+    cart = []
+    while best_buy_items:
+        purchase = input("Purchase a item:")
+        cart.append(purchase)
+        if input == 'TV':
+            print(cart, "Continue shopping or are you done?")
+        elif input == 'IPhone':
+            print(cart)
+        elif input == 'IPad':
+            print(cart)
+        elif input == continue:
+              
+        elif input == 'done':
+              print(cart)
+
 
 
 
